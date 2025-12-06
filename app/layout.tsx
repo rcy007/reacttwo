93346014721bdfdf324c56eb7b1b87aaadf86bd8
Abhinav Ahuja <ahujaaa@amazon.com>
2025-12-07 00:37:40 +0530

    Added next-themes, dark mode and shit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,26 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/others/app-sidebar";
+import { ThemeProvider } from "@/components/others/theme-provider";
 import "./globals.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
-    </body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
