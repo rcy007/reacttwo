4926b4dcd4e54b100ba7cd7dd83088565ba06659
Abhinav Ahuja <ahujaaa@amazon.com>
2025-12-07 01:22:27 +0530

    Navbar Toggle fixes and Dark Mode Toggle Layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/others/app-sidebar";
 import { ThemeProvider } from "@/components/others/theme-provider";
+import { ModeToggle } from "@/components/others/theme-toggle";
 import "./globals.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
+          // forcedTheme="light"
           enableSystem
           disableTransitionOnChange>
           <SidebarProvider>
             <AppSidebar />
-            <main>
+            <main className="flex-1">
+              <div className="flex items-center justify-between px-4 py-2">
               <SidebarTrigger />
+              <ModeToggle />
+              </div>
               {children}
             </main>
           </SidebarProvider>
